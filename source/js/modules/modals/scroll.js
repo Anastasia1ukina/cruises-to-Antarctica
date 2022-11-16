ee171090bb1141178b9d366801f13935449b7033
Anastasia1ukina <lukina02anastasiy@gmail.com>
2022-11-17 01:14:16 +0300

    Исправлено 8 багов

--- source/js/modules/modals/scroll.js
+++ source/js/modules/modals/scroll.js
@@ -1,9 +1,13 @@
 const anchors = document.querySelectorAll('a[href*="#"]');
+const pageBody = document.querySelector('body');
+let navMain = document.querySelector('.main-nav');
 
 for (let anchor of anchors) {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
-
+    pageBody.style.position = '';
+    navMain.classList.remove('main-nav--opened');
+    pageBody.classList.remove('body-overlay--background');
     const blockID = anchor.getAttribute('href').substr(1);
 
     document.getElementById(blockID).scrollIntoView({
